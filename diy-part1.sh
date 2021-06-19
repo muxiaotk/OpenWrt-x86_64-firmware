a62bc3ecff3f46a8cc97b882498cb1c7f16f001e
muxiaotk <muxiaotk@163.com>
2021-06-19 12:50:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,4 +14,8 @@
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# 微信推送插件
+git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
