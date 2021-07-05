2f622af04e5b4b2cf87c9e1d73f007105cd0aa16
muxiaotk <muxiaotk@163.com>
2021-07-05 13:20:26 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,6 +19,7 @@ sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 # 微信推送插件
 git clone https://github.com/tty228/luci-app-serverchan package/lean/luci-app-serverchan
-
+# 应用过滤
 git clone https://github.com/destan19/OpenAppFilter.git package/lean/OpenAppFilter
+# 京东签到
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus
