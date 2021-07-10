0943d675394897d88783c136efd5e9f06e73bf95
muxiaotk <muxiaotk@163.com>
2021-07-10 18:59:34 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,14 +11,16 @@
 #
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# 深造插件
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 # 微信推送插件
-#git clone https://github.com/tty228/luci-app-serverchan package/lean/luci-app-serverchan
+git clone https://github.com/tty228/luci-app-serverchan package/lean/luci-app-serverchan
 # 应用过滤
 git clone https://github.com/destan19/OpenAppFilter.git package/lean/OpenAppFilter
 # 京东签到
